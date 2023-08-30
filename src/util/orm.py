c60441f13d41af3c6a75248e0775f6e66c0e8105
Jordan Singer <jordan.singer@klo.dev>
2023-08-30 15:51:44 -0500

    have to change name of orm id

--- src/util/orm.py
+++ src/util/orm.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session, DeclarativeBase
 from sqlalchemy import create_engine
 
 # @klotho::persist {
-#   id = "storage"
+#   id = "ifcp-orm"
 # }
 engine = create_engine("sqlite://", echo=False, future=True)
 session = Session(engine)
