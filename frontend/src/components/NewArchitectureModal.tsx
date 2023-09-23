0111ac653ff72f2de74e8a7a5cc8f2f1c3ab43b2
DavidSeptimus-Klotho <david.septimus@klo.dev>
2023-09-22 18:50:09 -0600

    Automatically selects the default value when opening the New Architecture modal

--- frontend/src/components/NewArchitectureModal.tsx
+++ frontend/src/components/NewArchitectureModal.tsx
@@ -1,5 +1,6 @@
 import { Button, Label, Modal, TextInput } from "flowbite-react";
 import { useForm } from "react-hook-form";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 interface NewArchitectureModalProps {
   onClose?: () => void;
@@ -24,6 +25,27 @@ export default function NewArchitectureModal({
     formState: { errors },
   } = useForm<NewArchitectureFormState>();
 
+  // required for ref sharing with react-hook-form: https://www.react-hook-form.com/faqs/#Howtosharerefusage
+  const { ref, ...rest } = register("name", {
+    required: "Name is required",
+    maxLength: {
+      value: 80,
+      message: "Name must be at most 80 characters long",
+    },
+    onChange: async () => {
+      await trigger("name");
+    },
+  });
+
+  const [shouldSelectDefaultValue, setShouldSelectDefaultValue] =
+    useState(true);
+
+  useEffect(() => {
+    if (show) {
+      setShouldSelectDefaultValue(true);
+    }
+  }, [show]);
+
   return (
     <Modal
       show={show}
@@ -46,16 +68,15 @@ export default function NewArchitectureModal({
             </div>
             <TextInput
               id="name"
-              {...register("name", {
-                required: "Name is required",
-                maxLength: {
-                  value: 80,
-                  message: "Name must be at most 80 characters long",
-                },
-                onChange: async () => {
-                  await trigger("name");
-                },
-              })}
+              ref={(e) => {
+                if (shouldSelectDefaultValue) {
+                  e?.focus();
+                  e?.setSelectionRange(0, e?.value.length);
+                  setShouldSelectDefaultValue(false);
+                }
+                ref(e);
+              }}
+              {...rest}
               defaultValue="Untitled Architecture"
               type="text"
               color={errors.name ? "failure" : undefined}
