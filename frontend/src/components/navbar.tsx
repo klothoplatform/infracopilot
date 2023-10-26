3a858a1c36d79ff311eb02aa88a8baeb5995b957
DavidSeptimus-Klotho <david.septimus@klo.dev>
2023-10-26 16:05:00 -0600

    Cleans up the account menu dropdown on the navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,16 +1,11 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { type FC, type PropsWithChildren } from "react";
 import { DarkThemeToggle, Dropdown, Navbar } from "flowbite-react";
-import { HiSearch } from "react-icons/hi";
-import { useSidebarContext } from "../context/SidebarContext";
 import LoginButton from "../auth/Login";
 import useApplicationStore from "../views/store/ApplicationStore";
 
 interface NavbarProps {}
 
 const NavBar: FC<PropsWithChildren<NavbarProps>> = function ({ children }) {
-  const { isOpenOnSmallScreens, setOpenOnSmallScreens } = useSidebarContext();
-
   const { user, isAuthenticated } = useApplicationStore();
 
   return (
@@ -29,17 +24,10 @@ const NavBar: FC<PropsWithChildren<NavbarProps>> = function ({ children }) {
           <div className="w-full items-start">{children}</div>
           <div className="flex items-center lg:gap-3">
             <div className="flex items-center">
-              <button
-                onClick={() => setOpenOnSmallScreens(!isOpenOnSmallScreens)}
-                className="cursor-pointer rounded p-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:bg-gray-700 dark:focus:ring-gray-700 lg:hidden"
-              >
-                <span className="sr-only">Search</span>
-                <HiSearch className="h-6 w-6" />
-              </button>
               <DarkThemeToggle />
             </div>
             <div className="hidden lg:block">
-              {isAuthenticated && user ? <UserDropdown /> : <LoginButton />}
+              {isAuthenticated && user ? <AccountDropdown /> : <LoginButton />}
             </div>
           </div>
         </div>
@@ -48,7 +36,7 @@ const NavBar: FC<PropsWithChildren<NavbarProps>> = function ({ children }) {
   );
 };
 
-const UserDropdown: FC = function () {
+const AccountDropdown: FC = function () {
   const { user, logout } = useApplicationStore();
 
   if (!user) {
@@ -60,20 +48,26 @@ const UserDropdown: FC = function () {
       arrowIcon={false}
       inline
       label={
-        <span>
+        <span className="h-10 w-10 rounded-full">
           <span className="sr-only">User menu</span>
-          <img src={user.picture} alt={user.name} />
+          <img className="rounded-full" src={user.picture} alt="Account" />
         </span>
       }
     >
       <Dropdown.Header>
-        <div>
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
+        <div className="flex items-center gap-2">
+          <img
+            className="max-h-[4rem] max-w-[4rem] rounded-full"
+            src={user.picture}
+            alt="Account"
+          />
+          <div>
+            <h2 className="font-medium">{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
         </div>
       </Dropdown.Header>
-      <Dropdown.Item onClick={logout}>Log Out</Dropdown.Item>
+      <Dropdown.Item onClick={logout}>Log out</Dropdown.Item>
     </Dropdown>
   );
 };
