8ecd4d4abb4e184b9ece98490a1ebe689b478fbb
DavidSeptimus-Klotho <david.septimus@klo.dev>
2023-11-14 18:56:43 -0700

    Makes the big architecture button outlined

--- frontend/src/components/NewArchitectureBigButton.tsx
+++ frontend/src/components/NewArchitectureBigButton.tsx
@@ -1,4 +1,5 @@
 import { type FC, useState } from "react";
+import type { CustomFlowbiteTheme } from "flowbite-react";
 import { Button } from "flowbite-react";
 import NewArchitectureModal from "./NewArchitectureModal";
 import { BiPlus } from "react-icons/bi";
@@ -8,18 +9,34 @@ interface NewArchitectureButtonProps {
   onClick?: () => void;
 }
 
+const buttonTheme: CustomFlowbiteTheme["button"] = {
+  base: "group flex items-stretch items-center justify-center p-0.5 text-center font-medium relative focus:z-10 focus:outline-none",
+  fullSized: "w-full h-full",
+  inner: {
+    base: "flex items-stretch items-center transition-all duration-200 w-full h-full",
+  },
+};
+
 const NewArchitectureBigButton: FC<NewArchitectureButtonProps> = (
   props: NewArchitectureButtonProps,
 ) => {
   return (
-    <Button
-      color={"purple"}
-      className="h-[6rem] w-[5rem] rounded-lg p-8 drop-shadow-md"
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      <BiPlus className="h-[2.5rem] w-[2.5rem]" />
-    </Button>
+    <div className="h-[8rem] w-[6rem]">
+      <Button
+        aria-label="Create a New Architecture"
+        theme={buttonTheme}
+        className={
+          "[&>span]:text-primary-600 [&>span]:hover:text-white [&>span]:dark:text-primary-600 [&>span]:dark:hover:text-white"
+        }
+        color={"purple"}
+        onClick={props.onClick}
+        disabled={props.disabled}
+        outline
+        fullSized
+      >
+        <BiPlus className="h-[3rem] w-[3rem]" />
+      </Button>
+    </div>
   );
 };
 
