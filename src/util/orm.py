adc33bc6aa5fc8d50f0ee32a883f5b077d16b70d
Gordon <gordon.goetz@klo.dev>
2023-12-04 20:01:15 -0500

    Allow storing sqlite db for local dev with DB_PATH env var

--- src/util/orm.py
+++ src/util/orm.py
@@ -1,10 +1,16 @@
+import os
+
 from sqlalchemy.orm import Session, DeclarativeBase
 from sqlalchemy import create_engine
+
+db = os.getenv("DB_PATH", "")
+if db != "":
+    db = f"/{db}"
 
 # @klotho::persist {
 #   id = "ifcporm"
 # }
-engine = create_engine("sqlite://", echo=False, future=True)
+engine = create_engine(f"sqlite://{db}", echo=False, future=True)
 session = Session(engine)
 
 
