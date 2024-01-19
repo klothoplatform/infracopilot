899e669939998833425daee47e29fef77d31099c
Jordan Singer <jordan.singer@klo.dev>
2024-01-19 09:15:50 -0500

    add tests and gh workflow changes

--- tests/engine_service/engine_commands/test_get_resource_types.py
+++ tests/engine_service/engine_commands/test_get_resource_types.py
@@ -8,7 +8,7 @@ from unittest import mock
 from src.engine_service.engine_commands.get_resource_types import (
     get_resource_types,
 )
-from src.engine_service.binaries.fetcher import BinaryStorage
+from src.engine_service.binaries.fetcher import BinaryStorage, Binary
 
 
 class TestRunEngine(aiounittest.AsyncTestCase):
@@ -28,7 +28,7 @@ class TestRunEngine(aiounittest.AsyncTestCase):
             '{"aws:lambda_function": ["serverless", "compute"], "aws:rest_api": ["serverless", "api"]}',
             "",
         )
-        binary_store = BinaryStorage(bucket=None)
+        binary_store = mock.MagicMock()
         binary_store.ensure_binary = mock.Mock()
         result = get_resource_types(binary_store)
         mock_eng_cmd.assert_called_once_with("ListResourceTypes")
@@ -39,3 +39,4 @@ class TestRunEngine(aiounittest.AsyncTestCase):
             },
             json.loads(result),
         )
+        binary_store.ensure_binary.assert_called_once_with(Binary.ENGINE)
