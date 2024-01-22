a1a5752476d2cce7f01adef89a696d85612ef4cb
jhsinger-klotho <111291520+jhsinger-klotho@users.noreply.github.com>
2024-01-22 18:35:44 -0500

    update workers (#132)

--- container_start.sh
+++ container_start.sh
@@ -5,4 +5,4 @@ aws secretsmanager get-secret-value --secret-id $FGA_MODEL_ID --query SecretStri
 aws secretsmanager get-secret-value --secret-id $FGA_SECRET --query SecretString --output text | tr -d '\n' > fga_secret.key
 aws secretsmanager get-secret-value --secret-id $FGA_STORE_ID --query SecretString --output text | tr -d '\n' > fga_store_id.key
 
-exec uvicorn src.main:app --host '0.0.0.0' --port=80
+exec uvicorn src.main:app --host '0.0.0.0' --port=80 --workers=8
--- src/engine_service/engine_commands/util.py
+++ src/engine_service/engine_commands/util.py
@@ -83,28 +83,6 @@ def run_engine_command(*args, **kwargs) -> tuple[str, str]:
     print(f"Running engine command: {' '.join(cmd)}")
     log.debug("Running engine command: %s", " ".join(cmd))
 
-    print(
-        f"{Binary.ENGINE.path} exists {Binary.ENGINE.path.exists()} = {Binary.ENGINE.path.lstat()}"
-    )
-    try:
-        print("ls:\n" + subprocess.run(["ls", "-la", "/tmp"]).stdout)
-    except:
-        pass
-    try:
-        print(
-            "help:\n"
-            + subprocess.run(["sh", "-c", f"{Binary.ENGINE.path} --help"]).stdout
-        )
-    except:
-        pass
-    try:
-        print(
-            "stuff:\n"
-            + subprocess.run(["sh", "-c", f"ls /tmp; stat {Binary.ENGINE.path}"]).stdout
-        )
-    except:
-        pass
-
     result = subprocess.run(cmd, capture_output=True, cwd=cwd, env=env)
 
     out_logs = result.stdout.decode()
