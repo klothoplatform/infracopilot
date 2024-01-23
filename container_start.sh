6e55ecf35b75d125d69d5791cde66255a1ce4cb2
jhsinger-klotho <111291520+jhsinger-klotho@users.noreply.github.com>
2024-01-23 12:58:36 -0500

    inject fga creds (#144)

--- container_start.sh
+++ container_start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 aws secretsmanager get-secret-value --secret-id $FGA_CLIENT_ID --query SecretString --output text | tr -d '\n' > fga_client_id.key
-aws secretsmanager get-secret-value --secret-id $FGA_MODEL_ID --query SecretString --output text | tr -d '\n' > fga_model_id.key
 aws secretsmanager get-secret-value --secret-id $FGA_SECRET --query SecretString --output text | tr -d '\n' > fga_secret.key
-aws secretsmanager get-secret-value --secret-id $FGA_STORE_ID --query SecretString --output text | tr -d '\n' > fga_store_id.key
+aws secretsmanager get-secret-value --secret-id $AUTH0_CLIENT_ID --query SecretString --output text | tr -d '\n' > auth0_client_id.key
+aws secretsmanager get-secret-value --secret-id $AUTH0_SECRET --query SecretString --output text | tr -d '\n' > auth0_client_secret.key
 
 exec uvicorn src.main:app --host '0.0.0.0' --port=80 --workers=8
