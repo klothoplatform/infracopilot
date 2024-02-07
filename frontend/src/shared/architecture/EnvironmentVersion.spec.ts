0d073f9e8732d87be0265337024686222cb17975
Gordon <gordon.goetz@klo.dev>
2024-02-07 11:34:23 -0500

    Add unit test

---
+++ frontend/src/shared/architecture/EnvironmentVersion.spec.ts
@@ -0,0 +1,114 @@
+import { PrimitiveTypes, type Property } from "../resources/ResourceTypes";
+import { filterPromotedProperties } from "./EnvironmentVersion";
+
+describe("filterPromotedProperties", () => {
+  it("top level important doesn't filter", () => {
+    const input: Property = {
+      type: PrimitiveTypes.String,
+      name: "A",
+      qualifiedName: "A",
+      important: true,
+    };
+
+    const output = filterPromotedProperties(input);
+    expect(output).toEqual(input);
+  });
+
+  it("filtered subproperties", () => {
+    const input: Property = {
+      type: PrimitiveTypes.String,
+      name: "A",
+      qualifiedName: "A",
+      properties: [
+        {
+          type: PrimitiveTypes.String,
+          name: "B",
+          qualifiedName: "A.B",
+          important: false,
+        },
+        {
+          type: PrimitiveTypes.String,
+          name: "C",
+          qualifiedName: "A.C",
+          important: true,
+        },
+      ],
+    };
+
+    const output = filterPromotedProperties(input);
+    expect(output).toEqual({
+      ...input,
+      properties: [
+        {
+          type: PrimitiveTypes.String,
+          name: "C",
+          qualifiedName: "A.C",
+          important: true,
+        },
+      ],
+    });
+  });
+
+  it("filtered nested subproperties", () => {
+    const input: Property = {
+      type: PrimitiveTypes.String,
+      name: "A",
+      qualifiedName: "A",
+      properties: [
+        {
+          type: PrimitiveTypes.String,
+          name: "B",
+          qualifiedName: "A.B",
+          important: false,
+          properties: [
+            {
+              type: PrimitiveTypes.String,
+              name: "X",
+              qualifiedName: "A.B.X",
+              important: true,
+            },
+            {
+              type: PrimitiveTypes.String,
+              name: "Y",
+              qualifiedName: "A.B.Y",
+              important: false,
+            },
+          ],
+        },
+        {
+          type: PrimitiveTypes.String,
+          name: "C",
+          qualifiedName: "A.C",
+          important: true,
+        },
+      ],
+    };
+
+    const output = filterPromotedProperties(input);
+    expect(output).toEqual({
+      ...input,
+      properties: [
+        {
+          type: PrimitiveTypes.String,
+          name: "B",
+          qualifiedName: "A.B",
+          important: false,
+          properties: [
+            {
+              type: PrimitiveTypes.String,
+              name: "X",
+              qualifiedName: "A.B.X",
+              important: true,
+            },
+          ],
+        },
+        {
+          type: PrimitiveTypes.String,
+          name: "C",
+          qualifiedName: "A.C",
+          important: true,
+        },
+      ],
+    });
+  });
+});
