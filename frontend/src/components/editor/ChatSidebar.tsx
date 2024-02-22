bb2ea62888e1c0cafd4c430e773e161d5e395cb2
DavidSeptimus-Klotho <david.septimus@klo.dev>
2024-02-21 18:39:57 -0700

    Cleans up explain mentions when copying message content

--- frontend/src/components/editor/ChatSidebar.tsx
+++ frontend/src/components/editor/ChatSidebar.tsx
@@ -569,7 +569,10 @@ const Avatar: React.FC<AvatarProps> = ({
 };
 
 function resolveMentions(text: string) {
-  return text.replace(/<msft-mention id=".*?">(.*?)<\/msft-mention>/g, "$1");
+  return text
+    .replace(/<msft-mention id="resource#.*?">(.*?)<\/msft-mention>/g, "$1")
+    .replace(/<msft-mention id="explain#.*?">(.*?)<\/msft-mention>/g, "")
+    .trim();
 }
 
 const BottomBar: FC<{
